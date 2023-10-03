a4ac3c871dc35b0231a480cd158c0fd5fe720150
Matias <mandelbaum.matias@gmail.com>
2023-10-03 10:57:19 -0300

    Modified for MVVM arch. Added BookRepository interface, Firebase and RepositoryModule. Added Result class for UI. Modified SearchResultViewModel and it's fragment

---
+++ app/src/main/java/com/matiasmandelbaum/alejandriaapp/di/RepositoryModule.kt
@@ -0,0 +1,15 @@
+package com.matiasmandelbaum.alejandriaapp.di
+
+import com.matiasmandelbaum.alejandriaapp.data.firestorebooks.repository.BooksRepositoryImpl
+import com.matiasmandelbaum.alejandriaapp.domain.repository.BooksRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    abstract fun bindBooksRepository(booksRepository: BooksRepositoryImpl): BooksRepository
+}
--- app/src/main/java/com/matiasmandelbaum/alejandriaapp/ui/searchresult/SearchResultListFragment.kt
+++ app/src/main/java/com/matiasmandelbaum/alejandriaapp/ui/searchresult/SearchResultListFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.matiasmandelbaum.alejandriaapp.databinding.FragmentSearchResultListBinding
+import com.matiasmandelbaum.alejandriaapp.common.Result
 import dagger.hilt.android.AndroidEntryPoint
 
 private const val TAG = "SearchResultListFragment"
@@ -25,7 +26,7 @@ class SearchResultListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentSearchResultListBinding.inflate(inflater, container, false)
-        viewModel.fetchBooksResult()
+         //viewModel.getBooksByTitle()
 
         return binding.root
     }
@@ -34,5 +35,33 @@ class SearchResultListFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val args: SearchResultListFragmentArgs by navArgs()
         Log.d(TAG, args.titulo)
+        viewModel.getBooksByTitle()
+       // viewModel.getBooksByTitle()
+
+        viewModel.bookListState.observe(viewLifecycleOwner) {
+            when (it) {
+                is Result.Success ->{
+                    handleLoading(false)
+                    val className = it.data::class.java.simpleName
+                    Log.d(TAG, " mi data ${it.data}")
+                    Log.d(TAG, "The data type is: $className")
+                }
+                is Result.Loading -> handleLoading(true)
+                is Result.Error -> handleLoading(false)
+                else -> {
+
+                }
+            }
+        }
+    }
+
+    private fun handleLoading(isLoading: Boolean) {
+        with(binding) {
+            if (isLoading) {
+                progressBar.visibility = View.VISIBLE
+            } else {
+                progressBar.visibility = View.GONE
+            }
+        }
     }
 }
