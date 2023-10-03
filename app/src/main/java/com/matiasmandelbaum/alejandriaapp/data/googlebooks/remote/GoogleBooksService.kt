28eb4ebd7d6c0f5775cd31da44a56815c293d574
Matias <mandelbaum.matias@gmail.com>
2023-10-03 10:57:19 -0300

    Modified for MVVM arch. Added BookRepository interface, Firebase and RepositoryModule. Added Result class for UI. Modified SearchResultViewModel and it's fragment

---
+++ app/src/main/java/com/matiasmandelbaum/alejandriaapp/data/googlebooks/remote/GoogleBooksService.kt
@@ -0,0 +1,31 @@
+package com.matiasmandelbaum.alejandriaapp.data.googlebooks.remote
+
+import android.util.Log
+import com.matiasmandelbaum.alejandriaapp.core.googlebooks.GoogleBooksConfig
+import com.matiasmandelbaum.alejandriaapp.data.firestorebooks.model.BookFirestore
+import com.matiasmandelbaum.alejandriaapp.data.googlebooks.model.GoogleBooksResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+private const val TAG = "GoogleBooksService"
+
+class GoogleBooksService @Inject constructor(private val googleBooksApiClient: GoogleBooksApiClient) {
+    suspend fun searchBooksInGoogleBooks(booksFirestore: List<BookFirestore>): List<GoogleBooksResponse> {
+        return try {
+            val booksGoogleResponse = mutableListOf<GoogleBooksResponse>()
+            for (book in booksFirestore) {
+                val bookGoogleResponse = withContext(Dispatchers.IO) {
+                    googleBooksApiClient.searchBooksByISBN(book.isbn_13, GoogleBooksConfig.apiKey)
+                }
+                booksGoogleResponse.add(bookGoogleResponse)
+            }
+            return booksGoogleResponse
+        } catch (e: Exception) {
+            Log.d(TAG, "exception en google books $e")
+            emptyList()
+        }
+    }
+}
+
+
