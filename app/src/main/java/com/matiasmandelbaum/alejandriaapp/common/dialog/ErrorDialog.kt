9996625fd365a4c0f9019495ae13ed7e1d24c85b
Matias <mandelbaum.matias@gmail.com>
2023-10-10 15:22:05 -0300

    Added try/catch block for already existing email and Snackbar in SignInFragment

--- app/src/main/java/com/matiasmandelbaum/alejandriaapp/common/dialog/ErrorDialog.kt
+++ app/src/main/java/com/matiasmandelbaum/alejandriaapp/common/dialog/ErrorDialog.kt
@@ -7,68 +7,70 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.ViewGroup
+import androidx.core.view.isGone
 import androidx.fragment.app.DialogFragment
+import com.matiasmandelbaum.alejandriaapp.databinding.DialogErrorBinding
 
-//class ErrorDialog : DialogFragment() {
-//
-//    private var title: String = ""
-//    private var description: String = ""
-//    private var isDialogCancelable: Boolean = true
-//    private var positiveAction: Action = Action.Empty
-//    private var negativeAction: Action = Action.Empty
-//
-//    companion object {
-//        fun create(
-//            title: String = "",
-//            description: String = "",
-//            isDialogCancelable: Boolean = true,
-//            positiveAction: Action = Action.Empty,
-//            negativeAction: Action = Action.Empty,
-//        ): ErrorDialog = ErrorDialog().apply {
-//            this.title = title
-//            this.description = description
-//            this.isDialogCancelable = isDialogCancelable
-//            this.positiveAction = positiveAction
-//            this.negativeAction = negativeAction
-//        }
-//    }
-//
-//    override fun onStart() {
-//        super.onStart()
-//        val window = dialog?.window ?: return
-//
-//        window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-//        window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-//    }
-//
-//    @SuppressLint("InflateParams")
-//    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-//        val binding = DialogErrorBinding.inflate(requireActivity().layoutInflater)
-//
-//        binding.tvTitle.text = title
-//        binding.tvDescription.text = description
-//        if (negativeAction == Action.Empty) {
-//            binding.btnNegative.isGone = true
-//        } else {
-//            binding.btnNegative.text = negativeAction.text
-//            binding.btnNegative.setOnClickListener { negativeAction.onClickListener(this) }
-//        }
-//        binding.btnPositive.text = positiveAction.text
-//        binding.btnPositive.setOnClickListener { positiveAction.onClickListener(this) }
-//        isCancelable = isDialogCancelable
-//
-//        return AlertDialog.Builder(requireActivity())
-//            .setView(binding.root)
-//            .setCancelable(isDialogCancelable)
-//            .create()
-//    }
-//
-//    data class Action(
-//        val text: String,
-//        val onClickListener: (ErrorDialog) -> Unit
-//    ) {
-//        companion object {
-//            val Empty = Action("") {}
-//        }
-//    }
-//}
+class ErrorDialog : DialogFragment() {
+
+    private var title: String = ""
+    private var description: String = ""
+    private var isDialogCancelable: Boolean = true
+    private var positiveAction: Action = Action.Empty
+    private var negativeAction: Action = Action.Empty
+
+    companion object {
+        fun create(
+            title: String = "",
+            description: String = "",
+            isDialogCancelable: Boolean = true,
+            positiveAction: Action = Action.Empty,
+            negativeAction: Action = Action.Empty,
+        ): ErrorDialog = ErrorDialog().apply {
+            this.title = title
+            this.description = description
+            this.isDialogCancelable = isDialogCancelable
+            this.positiveAction = positiveAction
+            this.negativeAction = negativeAction
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val window = dialog?.window ?: return
+
+        window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    }
+
+    @SuppressLint("InflateParams")
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val binding = DialogErrorBinding.inflate(requireActivity().layoutInflater)
+
+        binding.tvTitle.text = title
+        binding.tvDescription.text = description
+        if (negativeAction == Action.Empty) {
+            binding.btnNegative.isGone = true
+        } else {
+            binding.btnNegative.text = negativeAction.text
+            binding.btnNegative.setOnClickListener { negativeAction.onClickListener(this) }
+        }
+        binding.btnPositive.text = positiveAction.text
+        binding.btnPositive.setOnClickListener { positiveAction.onClickListener(this) }
+        isCancelable = isDialogCancelable
+
+        return AlertDialog.Builder(requireActivity())
+            .setView(binding.root)
+            .setCancelable(isDialogCancelable)
+            .create()
+    }
+
+    data class Action(
+        val text: String,
+        val onClickListener: (ErrorDialog) -> Unit
+    ) {
+        companion object {
+            val Empty = Action("") {}
+        }
+    }
+}
