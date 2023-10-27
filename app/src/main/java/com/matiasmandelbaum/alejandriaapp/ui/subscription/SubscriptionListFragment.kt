610926847df4792e2943ae5e7baf8d98ab09c2f9
Matias Mandelbaum <mandelbaum.matias@gmail.com>
2023-10-27 08:11:44 -0300

    Added view visible and error message when user is logged out in SubscriptionListFragment. Added state LiveData to Auth Manager

--- app/src/main/java/com/matiasmandelbaum/alejandriaapp/ui/subscription/SubscriptionListFragment.kt
+++ app/src/main/java/com/matiasmandelbaum/alejandriaapp/ui/subscription/SubscriptionListFragment.kt
@@ -10,6 +10,9 @@ import android.widget.Toast
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.google.android.material.snackbar.Snackbar
+import com.matiasmandelbaum.alejandriaapp.R
+import com.matiasmandelbaum.alejandriaapp.common.auth.AuthManager
 import com.matiasmandelbaum.alejandriaapp.common.result.Result
 import com.matiasmandelbaum.alejandriaapp.databinding.FragmentSubscriptionListBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -35,6 +38,26 @@ class SubscriptionListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //  viewModel.getUserBySubscriptionId()
+
+        AuthManager.getAuthStateLiveData().observe(viewLifecycleOwner) { user ->
+            if (user != null) {
+                Log.d(TAG, "user logged $user")
+                val userUid = AuthManager.getCurrentUser()?.uid
+                userUid?.let {
+                    viewModel.getUserById(it)
+                }
+            } else {
+                binding.basicPlanSubscribeBtn.visibility = View.VISIBLE
+                binding.basicPlanSubscribeBtn.setOnClickListener {
+                    Snackbar.make(
+                        requireView(),
+                        "Iniciar sesión para subscribirse!", Snackbar.LENGTH_SHORT
+                    ).show()
+
+                }
+            }
+        }
+
 
         initObservers()
         initListeners()
