4de9f51f2610c661eaffb75cb3f3596af33603d7
LucasG <granellilucas@gmail.com>
2023-10-29 18:39:03 -0300

    user reserved book list added

---
+++ app/src/main/java/com/matiasmandelbaum/alejandriaapp/ui/booksreserved/Reserves.kt
@@ -0,0 +1,5 @@
+package com.matiasmandelbaum.alejandriaapp.ui.booksreserved
+
+import java.util.Date
+
+data class Reserves(var isbn : String, var title : String, var author : String, var reserveDate : String)
