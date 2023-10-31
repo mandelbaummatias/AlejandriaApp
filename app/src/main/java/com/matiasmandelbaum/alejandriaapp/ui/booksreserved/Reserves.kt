2b6829c3ad117943b7621ba0e172e70af5284647
LucasG <granellilucas@gmail.com>
2023-10-30 22:51:31 -0300

    sorting by date in reservation list added

--- app/src/main/java/com/matiasmandelbaum/alejandriaapp/ui/booksreserved/Reserves.kt
+++ app/src/main/java/com/matiasmandelbaum/alejandriaapp/ui/booksreserved/Reserves.kt
@@ -1,5 +1,7 @@
 package com.matiasmandelbaum.alejandriaapp.ui.booksreserved
 
-import java.util.Date
-
-data class Reserves(var isbn : String, var title : String, var author : String, var reserveDate : String)
+data class Reserves(var isbn : String, var title : String, var author : String, var reserveDate : String): Comparable<Reserves> {
+    override fun compareTo(other: Reserves): Int {
+        return other.reserveDate.compareTo(this.reserveDate)
+    }
+}
