13a78e3f9e6fb454524c4e5730655511b8581144
LucasG <granellilucas@gmail.com>
2023-11-01 21:29:25 -0300

    reserve list sort fix

--- app/src/main/java/com/matiasmandelbaum/alejandriaapp/ui/booksreserved/Reserves.kt
+++ app/src/main/java/com/matiasmandelbaum/alejandriaapp/ui/booksreserved/Reserves.kt
@@ -1,6 +1,8 @@
 package com.matiasmandelbaum.alejandriaapp.ui.booksreserved
 
-data class Reserves(var isbn : String, var title : String, var author : String, var reserveDate : String): Comparable<Reserves> {
+import java.util.Date
+
+data class Reserves(var isbn: String, var title: String, var author: String, var reserveDate: Date) : Comparable<Reserves> {
     override fun compareTo(other: Reserves): Int {
         return other.reserveDate.compareTo(this.reserveDate)
     }
