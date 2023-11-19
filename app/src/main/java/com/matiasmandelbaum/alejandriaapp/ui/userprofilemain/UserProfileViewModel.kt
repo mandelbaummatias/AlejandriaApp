40580d6d22594f9bf7ef955da3bc3878b21a1905
Matias Mandelbaum <105255748+mandelbaummatias@users.noreply.github.com>
2023-11-19 09:06:23 -0300

    Implementing MVVM arch in UserProfileFragment

---
+++ app/src/main/java/com/matiasmandelbaum/alejandriaapp/ui/userprofilemain/UserProfileViewModel.kt
@@ -0,0 +1,10 @@
+package com.matiasmandelbaum.alejandriaapp.ui.userprofilemain
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class UserProfileViewModel @Inject constructor(): ViewModel(){
+}
