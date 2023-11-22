5d23c1f2e55cdde5e32d14c2098a2e08487ac8dc
LucasG <granellilucas@gmail.com>
2023-11-22 20:12:06 -0300

    hotfix: reserve email update on user email change

--- app/src/main/java/com/matiasmandelbaum/alejandriaapp/ui/passwordconfirmation/PasswordConfirmationViewModel.kt
+++ app/src/main/java/com/matiasmandelbaum/alejandriaapp/ui/passwordconfirmation/PasswordConfirmationViewModel.kt
@@ -1,9 +1,12 @@
 package com.matiasmandelbaum.alejandriaapp.ui.passwordconfirmation
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.firestore.FirebaseFirestore
 import com.matiasmandelbaum.alejandriaapp.common.result.Result
 import com.matiasmandelbaum.alejandriaapp.domain.usecase.ChangeUserEmailUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -20,8 +23,35 @@ class PasswordConfirmationViewModel @Inject constructor(private val changeUserEm
         _onChangeUserEmailSuccess.value = Result.Loading
         viewModelScope.launch {
             val result = changeUserEmailUseCase(newEmail, previousEmail, pass)
+            if (result is Result.Success) {
+                changeUserEmailInReserves(newEmail, previousEmail)
+            }
             _onChangeUserEmailSuccess.postValue(result)
         }
+    }
+
+    private fun changeUserEmailInReserves(newEmail: String,oldEmail: String) {
+        val db = FirebaseFirestore.getInstance()
+        val reservesCollection = db.collection("reservas_libros")
+
+        reservesCollection.whereEqualTo("mail_usuario", oldEmail)
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                if (!querySnapshot.isEmpty) {
+                    for (document in querySnapshot.documents) {
+                        val reserveDocument = document.reference
+                        reserveDocument.update("mail_usuario", newEmail)
+                            .addOnSuccessListener {
+                                Log.d(TAG, "Actualización exitosa")
+                            }
+                            .addOnFailureListener { e ->
+                                Log.e(TAG, "Error al actualizar")
+                            }
+                    }
+                } else {
+                    Log.d(TAG, "No hay reservas para actualizar.")
+                }
+            }
     }
 
 }
