bef23e069ca58659e6d77a23aaba1040c500f6c3
Matias <mandelbaum.matias@gmail.com>
2023-11-24 10:49:38 -0300

    Manifest fix and SplashScreen refactor

--- app/src/main/java/com/matiasmandelbaum/alejandriaapp/common/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/matiasmandelbaum/alejandriaapp/common/splashscreen/SplashScreen.kt
@@ -2,28 +2,46 @@ package com.matiasmandelbaum.alejandriaapp.common.splashscreen
 
 import android.animation.ValueAnimator
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.WindowInsets
 import android.view.WindowManager
-import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
-import com.matiasmandelbaum.alejandriaapp.R
+import com.matiasmandelbaum.alejandriaapp.databinding.ActivitySplashScreenBinding
 import com.matiasmandelbaum.alejandriaapp.ui.MainActivity
 
-@Suppress("DEPRECATION")
-class SplashScreen : AppCompatActivity() {
+
+class FirstScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    companion object {
+        private const val PROGRESS_START = 0
+        private const val PROGRESS_END = 100
+        private const val ANIMATION_DURATION = 3000L
+        private const val DELAY_TIME = 3000L
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val progressBar = findViewById<ProgressBar>(R.id.progress_bar_splash_screen)
-        val progressAnimator = ValueAnimator.ofInt(0, 100)
-        progressAnimator.duration = 3000
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        val progressBar = binding.progressBarSplashScreen
+        val progressAnimator = ValueAnimator.ofInt(PROGRESS_START, PROGRESS_END)
+        progressAnimator.duration = ANIMATION_DURATION
         progressAnimator.addUpdateListener { animation ->
             progressBar.progress = animation.animatedValue as Int
         }
@@ -33,6 +51,6 @@ class SplashScreen : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000)
+        }, DELAY_TIME)
     }
 }
